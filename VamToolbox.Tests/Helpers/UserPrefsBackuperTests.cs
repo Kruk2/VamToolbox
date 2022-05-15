6c6c25d092b1b15cadda3adb8433ecae08325570
Kruk2 <Kruk2>
2022-05-15 15:43:12 +0200

    Implement .prefs file backuper

---
+++ VamToolbox.Tests/Helpers/UserPrefsBackuperTests.cs
@@ -0,0 +1,103 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using FluentAssertions;
+using NSubstitute;
+using VamToolbox.Helpers;
+using VamToolbox.Logging;
+using Xunit;
+
+namespace VamToolbox.Tests.Helpers;
+public class UserPrefsBackuperTests 
+{
+    private readonly MockFileSystem _fs;
+    private readonly UserPrefsBackuper _backuper;
+    private const string VamDir = "C:/VaM/";
+    private const string PrefsDir = VamDir + "AddonPackagesUserPrefs/";
+
+    public UserPrefsBackuperTests()
+    {
+        _fs = new MockFileSystem(new Dictionary<string, MockFileData> {
+            [PrefsDir + "a.prefs"] = new ("test"),
+            [PrefsDir + "b.prefs"] = new ("test2")
+        });
+        var logger = Substitute.For<ILogger>();
+        _backuper = new UserPrefsBackuper(_fs, logger);
+    }
+
+    [Fact]
+    public void Backup_WhenUserPrefsDirNotExists_ShouldSkip()
+    {
+        _backuper.Backup(PrefsDir, false);
+
+        _fs.AllFiles.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public void Backup_ShouldBackupBothFiles()
+    {
+        _backuper.Backup(VamDir, false);
+
+        _fs.AllFiles.Should().HaveCount(4);
+        _fs.GetFile(PrefsDir + "a.prefs").TextContents.Should().Be(_fs.GetFile(PrefsDir + "a.prefs.toolboxbak").TextContents);
+        _fs.GetFile(PrefsDir + "b.prefs").TextContents.Should().Be(_fs.GetFile(PrefsDir + "b.prefs.toolboxbak").TextContents);
+    }
+
+
+    [Fact]
+    public void Backup_WhenDryRun_ShouldSkipBackup()
+    {
+        _backuper.Backup(VamDir, true);
+
+        _fs.AllFiles.Should().HaveCount(2);
+        _fs.FileExists(PrefsDir + "a.prefs");
+        _fs.FileExists(PrefsDir + "b.prefs");
+    }
+
+    [Fact]
+    public void Backup_WhenBackupExitst_ShouldOverrideIt()
+    {
+        _fs.AddFile(PrefsDir + "a.prefs.toolboxbak", new MockFileData("old_backup"));
+        _backuper.Backup(VamDir, false);
+
+        _fs.AllFiles.Should().HaveCount(4);
+        _fs.GetFile(PrefsDir + "a.prefs").TextContents.Should().Be(_fs.GetFile(PrefsDir + "a.prefs.toolboxbak").TextContents);
+        _fs.GetFile(PrefsDir + "b.prefs").TextContents.Should().Be(_fs.GetFile(PrefsDir + "b.prefs.toolboxbak").TextContents);
+    }
+
+    [Fact]
+    public void Restore_WhenUserPrefsDirNotExists_ShouldSkip()
+    {
+        _backuper.Restore(PrefsDir, false);
+
+        _fs.AllFiles.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public void Restore_ShouldRestoreBothFiles()
+    {
+        _fs.AddFile(PrefsDir + "a.prefs.toolboxbak", new MockFileData("backup"));
+        _fs.AddFile(PrefsDir + "b.prefs.toolboxbak", new MockFileData("backup1"));
+
+        _backuper.Restore(VamDir, false);
+
+        _fs.AllFiles.Should().HaveCount(4);
+        _fs.GetFile(PrefsDir + "a.prefs").TextContents.Should().Be("backup");
+        _fs.GetFile(PrefsDir + "b.prefs").TextContents.Should().Be("backup1");
+        _fs.GetFile(PrefsDir + "a.prefs.toolboxbak").TextContents.Should().Be("backup");
+        _fs.GetFile(PrefsDir + "b.prefs.toolboxbak").TextContents.Should().Be("backup1");
+    }
+
+    [Fact]
+    public void Restore_WhenDryRun_ShouldSkipRestore()
+    {
+        _fs.AddFile(PrefsDir + "a.prefs.toolboxbak", new MockFileData("backup"));
+        _fs.AddFile(PrefsDir + "b.prefs.toolboxbak", new MockFileData("backup1"));
+
+        _backuper.Restore(VamDir, true);
+
+        _fs.AllFiles.Should().HaveCount(4);
+        _fs.GetFile(PrefsDir + "a.prefs").TextContents.Should().Be("test");
+        _fs.GetFile(PrefsDir + "b.prefs").TextContents.Should().Be("test2");
+        _fs.GetFile(PrefsDir + "a.prefs.toolboxbak").TextContents.Should().Be("backup");
+        _fs.GetFile(PrefsDir + "b.prefs.toolboxbak").TextContents.Should().Be("backup1");
+    }
+}
---
+++ VamToolbox/Helpers/UserPrefsBackuper.cs
@@ -0,0 +1,64 @@
+﻿using System.IO.Abstractions;
+using VamToolbox.Logging;
+
+namespace VamToolbox.Helpers;
+public interface IUserPrefsBackuper
+{
+    public void Backup(string vamDir, bool dryRun);
+    public void Restore(string vamDir, bool dryRun);
+}
+
+public class UserPrefsBackuper : IUserPrefsBackuper
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly ILogger _logger;
+    private const string BackupExtension = ".toolboxbak";
+
+    public UserPrefsBackuper(IFileSystem fileSystem, ILogger logger)
+    {
+        _fileSystem = fileSystem;
+        _logger = logger;
+    }
+
+    public void Backup(string vamDir, bool dryRun)
+    {
+        var userPrefsDir = UserPrefsDir(vamDir);
+        if (!_fileSystem.Directory.Exists(userPrefsDir)) {
+            _logger.Log("UserPrefsDir doesn't exist. Exiting");
+            return;
+        }
+
+        foreach (var file in _fileSystem.Directory.EnumerateFiles(userPrefsDir, "*.prefs")) {
+            var fileName = _fileSystem.Path.GetFileName(file) + BackupExtension;
+            var backupDestination = _fileSystem.Path.Combine(userPrefsDir, fileName);
+
+            if (!dryRun) {
+                _fileSystem.File.Copy(file, backupDestination, true);
+            }
+
+            _logger.Log($"Backing up {file} to {backupDestination}");
+        }
+    }
+
+    public void Restore(string vamDir, bool dryRun)
+    {
+        var userPrefsDir = UserPrefsDir(vamDir);
+        if (!_fileSystem.Directory.Exists(userPrefsDir)) {
+            _logger.Log("UserPrefsDir doesn't exist. Exiting");
+            return;
+        }
+
+        foreach (var file in _fileSystem.Directory.EnumerateFiles(userPrefsDir, "*" + BackupExtension)) {
+            var fileName = _fileSystem.Path.GetFileNameWithoutExtension(file);
+            var restoreDestination = _fileSystem.Path.Combine(userPrefsDir, fileName);
+
+            if (!dryRun) {
+                _fileSystem.File.Copy(file, restoreDestination, true);
+            }
+
+            _logger.Log($"Restoring {file} to {restoreDestination}");
+        }
+    }
+
+    private string UserPrefsDir(string vamDir) => _fileSystem.Path.Combine(vamDir, "AddonPackagesUserPrefs");
+}
