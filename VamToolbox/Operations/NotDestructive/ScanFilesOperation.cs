147b3fcf831231bd75e61774ca2e3bc8279abb77
Kruk2 <Kruk2>
2023-01-06 23:49:02 +0100

    fix: exclude symlinks from BrowserAssist plugin. fixes #3

--- VamToolbox/Operations/NotDestructive/ScanFilesOperation.cs
+++ VamToolbox/Operations/NotDestructive/ScanFilesOperation.cs
@@ -80,7 +80,7 @@ public sealed class ScanFilesOperation : IScanFilesOperation
         var isVamDir = _context.VamDir == rootDir;
         var files = _fs.Directory
             .EnumerateFiles(searchDir, "*.*", SearchOption.AllDirectories)
-            .Where(f => !f.Contains(@"\."))
+            .Where(f => !f.Contains(@"\.") && !f.NormalizePathSeparators().Contains(@"/Saves/PluginData/JayJayWon/BrowserAssist"))
             .Select(f => (path: f, softLink: _softLinker.GetSoftLink(f)))
             .Where(f => f.softLink is null || File.Exists(f.softLink))
             .Select(f => (f.path, fileInfo: _fs.FileInfo.New(f.softLink ?? f.path), f.softLink))
